b4330e5c68def7480e8f17eb5d1906dd28ae271c
marita1234567 <m.bibichadze@sabauni.edu.ge>
2024-11-17 22:01:42 +0400

    homework21

---
+++ script.js
@@ -0,0 +1,101 @@
+for (let index = 0; index < 16; index++) {
+    if(index >5)
+        console.log ( index +  "დიდია 5-ზე")
+    
+}
+
+let  nums = [3, 6, 9, 12, 15]
+for (let index = 0; index < nums.length; index++) {
+    if (nums[index] % 2 ===0)
+        console.log(nums[index])
+    
+}
+
+let word = "you love carrots"
+for (let index = 0; index < word.length; index++) {
+    if (word[index] === "a"){
+        console.log(word[index] + " სიტყვა შეიცავს ა ასოს ") 
+    }
+    
+}
+
+let words = ["red", "green", "blue", "yellow"]
+for (let index = 0; index < words.length; index++) {
+    if (words[index] > 5) {
+        console.log(words[index]);
+    }
+    
+    
+}
+
+for (let index = 0; index < 50; index++) {
+    if(index %7 ===0)
+    console.log(index + "ეს რიცხვი იყოფა 7-ზე")
+}
+
+let gamrvleba = [4, 8, 12, 16, 20]
+for (let index = 0; index < gamrvleba.length; index++) {
+console.log (gamrvleba[index] * 2)
+    
+}
+
+let word1 = "javascript";
+
+for (let index = 0; index < word1.length; index++) {
+  if (word1[index] === "j") {
+    break; 
+  }
+  console.log(word1[i]);
+}
+
+let  zero=  [5, 10, 15, 20, 25]
+for (let index = 0; index < zero.length; index++) {
+    if (zero[index] % 5 ===0 )
+    console.log(zero[index] )
+    
+}
+
+
+for (let index = 1; index <= 10; index++) {
+    console.log(`10 * ${index} = ` + 10 * index);
+    
+}
+
+let woo = [2, 7, 4, 9, 5] 
+for (let index = 0; index < woo.length; index++) {
+    if(woo[index]> 4) {
+        console.log(woo[index] * 3)
+    }
+        
+    
+}
+for (let index = 0; index < 30; index++) {
+    if (index > 10){
+        console.log (index + "es ricxvi 10-ze metia")
+    }
+    
+}
+
+let word2 = "frontend"
+for (let index = 0; index < word2.length; index++) {
+    if (word2[index] ==="e"){
+        console.log(word2[index]+ "ნაპოვნია e ასო")
+    }
+    
+}
+let numberss= [100, 200, 300, 400, 500] 
+for (let index = 0; index < numberss.length; index++) {
+    if (numberss[index] < 250){
+        console.log(numberss[index] + "ეს რიცხვები ნაკლებია 250ზე ")
+    }
+    
+}
+
+
+let number7 =[4, 3, 8, 9, 1];
+for (let index = 0; index < number7.length; index++) {
+   console.log(number7[index]* 2)
+    
+}
+
+
